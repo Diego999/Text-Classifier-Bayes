8aa9ed5282784d6a7664b838119b497d0aba68d7
Diego Antognini <diegoantognini@gmail.com>
2014-04-05 16:44:27 +0200

    create settings file

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATA_PATH_POS = 'data/normal/pos'
+DATA_TAGGED_PATH_POS = 'data/tagged/pos'
+DATA_PATH_NEG = 'data/normal/neg'
+DATA_TAGGED_PATH_NEG = 'data/tagged/neg'
+STOP_WORD_FILEPATH = 'data/frenchST.txt'
+
+STOP_WORDS = []
+"""with open(STOP_WORD_FILEPATH, 'r') as f:
+    for l in f.read().splitlines():
+        STOP_WORDS.append(l.decode('utf-8'))"""
